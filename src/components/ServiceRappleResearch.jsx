48f7b6e8715829923d07acc436a23b2a981d2310
Muhammad Zeeshan <mzeeshan@godats.com>
2025-12-22 18:02:22 +0500

    Overflow x hidden

--- src/components/ServiceRappleResearch.jsx
+++ src/components/ServiceRappleResearch.jsx
@@ -44,7 +44,7 @@ const ServiceRappleResearch = ({ highlightedTitle, title, description }) => {
       {/* ✅ Your section */}
       <section
         ref={sectionRef}
-        className="relative py-20 md:min-h-screen min-h-auto z-10 overflow-hidden"
+        className="relative py-20 md:min-h-screen min-h-auto z-10 overflow-x-hidden"
       >
         {/* ✅ Overlay above bg */}
         <div className="absolute inset-0 bg-tertiary_color/90 z-0" />
